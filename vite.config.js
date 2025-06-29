3f4c08bf18d21eea7e54631183e97e16d4c79a1b
lamlib <lamlib2023@gmail.com>
2025-06-29 09:18:08 -0700

    style: home

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,21 @@ export default defineConfig({
   esbuild: {
     minify: true,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        addArticle: './pages/add-article.html',
+        allArticle: './pages/all-article.html',
+        editArticle: './pages/edit-article.html',
+        emailConfirm: './pages/email-confirm.html',
+        login: './pages/login.html',
+        home: './pages/home.html',
+        profile: './pages/profile.html',
+        viewArticle: './pages/view-article.html',
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
