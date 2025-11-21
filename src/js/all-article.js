765fc497ea6ef87c8ed3d13dea89095a9567a45c
lamlib <lamlib2023@gmail.com>
2025-11-21 19:50:50 +0700

    feat: update dark theme for edit and all article page

--- src/js/all-article.js
+++ src/js/all-article.js
@@ -1,5 +1,7 @@
 import { hasError, requestHandlers } from '@lamlib/data-sync';
 import { getOneElementOrFail } from './utils';
+import Pagination from '@lamlib/pagination';
+import DOMPurify from 'dompurify';
 
 const app = (function() {
     const state = {
@@ -13,54 +15,38 @@ const app = (function() {
         get deleteModal () {
             return getOneElementOrFail('#deleteModal');
         },
+        get articlesPagination () {
+            return getOneElementOrFail('#articleTablePagination');
+        }
     }
 
     const { getArticles, deleteArticleById } = requestHandlers;
 
     function _drawArticlesTable(articles) {
-        ui.articlesTable.tBodies.item(0).innerHTML = ''; // Clear existing rows
-        if(articles.length === 0) {
-            const row = document.createElement('tr');
-            const td = document.createElement('td');
-            td.setAttribute('colspan', '6');
-            td.classList.add('text-center', 'py-4');
-            td.textContent = 'Không có bài viết nào.';
-            row.appendChild(td);
-            ui.articlesTable.tBodies.item(0).appendChild(row);
-            return;
-        }
-        articles.forEach((element, idx) => {
-            const row = document.createElement('tr');
-            const td1 = document.createElement('td');
-            const td2 = document.createElement('td');
-            const td3 = document.createElement('td');
-            const td4 = document.createElement('td');
-            const td5 = document.createElement('td');
-            const td6 = document.createElement('td');
-            const rowStyle = ['px-6', 'py-2'];
-            td1.classList.add(...rowStyle);
-            td2.classList.add(...rowStyle);
-            td3.classList.add(...rowStyle);
-            td4.classList.add(...rowStyle);
-            td5.classList.add(...rowStyle);
-            td6.classList.add(...rowStyle);
-            td1.textContent = idx + 1;
-            td2.textContent = element.title;
-            td3.innerHTML = element.tags.map(tag => 
-                `<span class="bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200 px-3 py-1 rounded-full text-xs cursor-pointer hover:opacity-80">${tag}</span>`
-            ).join(' ');
-            td4.innerHTML = `<img src="${element.thumbnail}" class="w-16 h-16 object-cover rounded" alt="Thumbnail" />`;
-            td5.innerHTML = `<a href="/pages/view-article?id=${element.id}" class="text-blue-500 hover:text-blue-700">Xem</a>`;
-            td6.innerHTML = `<a href="/pages/edit-article?id=${element.id}" class="text-orange-500 hover:text-orange-700 mr-2">Sửa</a>`;
-            td6.innerHTML += ` <button data-id="${element.id}" class="text-red-500 hover:text-red-700 deleteBtn">Xoá</button>`;
-            row.appendChild(td1);
-            row.appendChild(td2);
-            row.appendChild(td3);
-            row.appendChild(td4);
-            row.appendChild(td5);
-            row.appendChild(td6);
-            ui.articlesTable.tBodies.item(0).appendChild(row);
-        });
+        new Pagination({
+            tableId: ui.articlesTable.id,
+            data: articles,
+            itemsPerPage: 5,
+            paginationContainerId: ui.articlesPagination.id,
+            rowRenderer: (idx, article) => {
+                const Tags = article.tags.map(tag => `<span class="bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200 px-3 py-1 rounded-full text-xs cursor-pointer hover:opacity-80">${DOMPurify.sanitize(tag)}</span>`).join(' ');
+                const Thumbnail = `<img src="${DOMPurify.sanitize(article.thumbnail)}" class="w-16 h-16 object-cover rounded" alt="Thumbnail" />`
+                const BtnView = `<a href="/pages/view-article?id=${DOMPurify.sanitize(article.id)}" class="text-blue-500 hover:text-blue-700">Xem</a>`;
+                const BtnEdit = `<a href="/pages/edit-article?id=${DOMPurify.sanitize(article.id)}" class="text-orange-500 hover:text-orange-700 mr-2">Sửa</a>`;
+                const BtnDelete = `<button data-id="${DOMPurify.sanitize(article.id)}" class="text-red-500 hover:text-red-700 deleteBtn">Xoá</button>`;
+                
+                return `
+                <tr>
+                    <td class="px-6 py-2">${DOMPurify.sanitize(idx + 1)}</td>
+                    <td class="px-6 py-2">${DOMPurify.sanitize(article.title)}</td>
+                    <td class="px-6 py-2">${Tags}</td>
+                    <td class="px-6 py-2">${Thumbnail}</td>
+                    <td class="px-6 py-2">${BtnView}</td>
+                    <td class="px-6 py-2">${BtnEdit + BtnDelete}</td>
+                </tr>
+                `;
+            }
+        })
     }
 
     async function _loadArticles (time = new Date().getTime()) {
